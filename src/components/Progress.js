62872c5556f95e355e3fea06b2ebd5c6d11e10af
GeorgioWan <georgio_sky2000@hotmail.com>
2017-04-11 17:21:42 +0000

    Ajust Progress.js

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -6,23 +6,23 @@ export default class Progress extends Component {
         const { stepsData, activeStep, stepsTotal } = this.props;
         const color_gold = '#e5b560',
               color_gray = '#3e4852';
+        const ua = navigator.userAgent.toLowerCase();
+        const progressWidth = ( ua.search( /(iphone)|(ipod)|(android)/ ) === -1 ) ? 0.2 : 1;
         let currentStepIndex = Object.keys(stepsData).findIndex((s)=> s === activeStep.id) + 1;
         let progress = parseInt(currentStepIndex/stepsTotal * 100, 10);
         
         return (
-            <div style={{position:'absolute', bottom:9, width:'100%', height:3}}>
-                <div style={{position: 'relative'}}>
-                    <Line percent={progress} 
-                          strokeLinecap='square'
-                          strokeWidth={.2} strokeColor={ color_gold }
-                          trailWidth={.2} trailColor={ color_gray } />
-                    <p style={{position:'absolute', bottom:14, fontSize:20, color: color_gray, width:'100%', textAlign:'center', opacity: .5}}>
-                        <span>
-                            { currentStepIndex }
-                            <span style={{paddingLeft:1, fontSize:13}}>{'/' + stepsTotal}</span>
-                        </span>
-                    </p>
-                </div>
+            <div style={{position:'fixed', bottom:'-3px', width:'100%'}}>
+                <p style={{fontSize:20, color: color_gray, textAlign:'center', opacity: .5}}>
+                    <span>
+                        { currentStepIndex }
+                        <span style={{paddingLeft:1, fontSize:13}}>{'/' + stepsTotal}</span>
+                    </span>
+                </p>
+                <Line percent={progress} 
+                      strokeLinecap='square'
+                      strokeWidth={ progressWidth } strokeColor={ color_gold }
+                      trailWidth={ progressWidth } trailColor={ color_gray } />
             </div>
         );
     }
